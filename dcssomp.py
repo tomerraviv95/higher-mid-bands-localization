43b8bf00e10ac5d18b592da77a99589cbf8c9663
tomer <tomerraviv95@gmail.com>
2024-01-10 13:15:42 +0200

    A working version of the code in python

--- dcssomp.py
+++ dcssomp.py
@@ -11,21 +11,22 @@ def DCSSOMP(Y, A, L):
 
     # 1. Initialization
     R = Y  # residual
-    psi = np.zeros((N, L, K))
+    psi = np.zeros((N, L, K), dtype=complex)
     indices = np.zeros(L, dtype=int)
-    columns = np.zeros((N, L, K))
-    betamatrix = np.zeros((L, K))
+    columns = np.zeros((N, L, K), dtype=complex)
+    betamatrix = np.zeros((L, K), dtype=complex)
 
     for counter in range(1, L + 1):
         # 2. Find maximum correlation between residual and columns of A
         cost = np.zeros(M)
-        for m in range(1, M + 1):
-            for k in range(1, K + 1):
+        for m in range(M):
+            for k in range(K):
                 # Calculate cost for each column m of A and each channel k
-                cost[m - 1] = cost[m - 1] + np.abs(np.dot(A[:, m - 1, k - 1].T, R[:, k - 1])) / np.linalg.norm(A[:, m - 1, k - 1])
+                cost[m] = cost[m] + np.abs(np.dot(A[:, m, k].conj().T, R[:, k])) / np.linalg.norm(A[:, m, k],2)
 
         maxi = np.argmax(cost)
-        indices[counter - 1] = maxi + 1
+        print(maxi)
+        indices[counter - 1] = maxi
 
         for k in range(1, K + 1):
             # 3. Orthogonalize
@@ -36,19 +37,19 @@ def DCSSOMP(Y, A, L):
             for counter2 in range(1, counter):
                 # Gram-Schmidt orthogonalization
                 psi[:, counter - 1, k - 1] = psi[:, counter - 1, k - 1] - (
-                        np.dot(psi[:, counter2 - 1, k - 1].T, omega) * psi[:, counter2 - 1, k - 1] /
+                        np.dot(psi[:, counter2 - 1, k - 1].conj().T, omega) * psi[:, counter2 - 1, k - 1] /
                         np.linalg.norm(psi[:, counter2 - 1, k - 1]) ** 2)
 
             # 4. Update coefficients and residual
-            beta = np.dot(psi[:, counter - 1, k - 1].T, R[:, k - 1]) / np.linalg.norm(psi[:, counter - 1, k - 1]) ** 2
+            beta = np.dot(psi[:, counter - 1, k - 1].conj().T, R[:, k - 1]) / np.linalg.norm(psi[:, counter - 1, k - 1]) ** 2
             betamatrix[counter - 1, k - 1] = beta
             R[:, k - 1] = R[:, k - 1] - beta * psi[:, counter - 1, k - 1]
 
     # 6. Deorthogonalize
-    h = np.zeros((L, K))
+    h = np.zeros((L, K), dtype=complex)
     for k in range(1, K + 1):
         # QR factorization
         Q, Rqr = np.linalg.qr(columns[:, :, k - 1], mode='reduced')
-        h[:, k - 1] = np.linalg.inv(Rqr) @ Q.T @ psi[:, :, k - 1] @ betamatrix[:, k - 1]
+        h[:, k - 1] = np.linalg.inv(Rqr) @ Q.conj().T @ psi[:, :, k - 1] @ betamatrix[:, k - 1]
 
     return indices, h
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from dcssomp import DCSSOMP
+
+np.random.seed(503)
+
 
 # Function to get response for a given number of antennas and spatial frequency
 def getResponse(antennas, phi):
@@ -8,7 +12,7 @@ def getResponse(antennas, phi):
 
 
 # System parameters
-L = 4  # number of paths (including LOS)
+L = 3  # number of paths (including LOS)
 Rs = 100  # total BW in MHz
 N = 10  # number of subcarriers
 Nt = 32  # number of TX antennas
@@ -17,8 +21,8 @@ Nb = Nt * 2  # number of beams in dictionary
 Ns = 20  # number of beams sent
 c = 300  # speed of light meter / us
 Ts = 1 / Rs  # sampling period in us
-posRx = np.array([5, 1])  # RX (user) position, TX is assumed to be in [0, 0]
-alpha = 0.2  # user orientation
+posRx = np.array([5, 5])  # RX (user) position, TX is assumed to be in [0, 0]
+alpha = 0  # user orientation
 sigma = 0.1  # noise standard deviation
 
 # Generate scatter points
@@ -40,8 +44,8 @@ for l in range(1, L):
 h = 10 * np.ones(L)  # some high channel gains
 
 # Create dictionary
-Ut = np.zeros((Nt, Nb))
-Ur = np.zeros((Nr, Nb))
+Ut = np.zeros((Nt, Nb), dtype=complex)
+Ur = np.zeros((Nr, Nb), dtype=complex)
 aa = np.arange(-Nb / 2, Nb / 2)
 aa = 2 * aa / Nb  # dictionary of spatial frequencies
 
@@ -54,29 +58,63 @@ H = np.zeros((Nr, Nt, N), dtype=complex)
 for n in range(N):
     for l in range(L):
         H[:, :, n] += h[l] * np.exp(-1j * 2 * np.pi * TOA[l] * n / (N * Ts)) * np.sqrt(Nr) * \
-                      getResponse(Nr, np.sin(AOA[l])) * np.sqrt(Nt) * getResponse(Nt, np.sin(AOD[l])).conj().T
+                      np.expand_dims(getResponse(Nr, np.sin(AOA[l])), axis=1) * np.sqrt(Nt) * np.expand_dims(
+            getResponse(Nt, np.sin(AOD[l])).conj().T, axis=0)
 
 # Visualize the beamspace channel for 1 subcarrier in AOA/AOD space
 Hb = np.dot(np.dot(Ur.conj().T, H[:, :, 0]), Ut)
 fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.contour3D(np.arcsin(aa), np.arcsin(aa), abs(Hb), 50, cmap='binary')
+plt.contourf(np.arcsin(aa), np.arcsin(aa), abs(Hb), cmap='magma')
+# ax.contour3D(, 50, cmap='binary')
+ax = plt.gca()
 ax.set_xlabel('AOD')
 ax.set_ylabel('AOA')
-plt.show()
+# plt.show()
 
 # Generate the observation and beamformers
 y = np.zeros((Nr, Ns, N), dtype=complex)
 F = np.zeros((Nt, Ns, N), dtype=complex)
 for k in range(Ns):
     for n in range(N):
-        F[:, k, n] = np.exp(1j * np.random.rand(Nt) * 2 * np.pi)  # random beamformers
+        F[:, k, n] = np.exp(
+            1j * np.random.rand(Nt) * 2 * np.pi)  # np.exp(1j * np.arange(1,Nt+1)/Nt * 2 * np.pi)  # random beamformers
         y[:, k, n] = np.dot(H[:, :, n], F[:, k, n]) + sigma / np.sqrt(2) * (
                 np.random.randn(Nr) + 1j * np.random.randn(Nr))
-
 # Vectorize and generation of the basis
 ybb = np.zeros((Nr * Ns, N), dtype=complex)
 Omega = np.zeros((Nr * Ns, Nb * Nb, N), dtype=complex)
 for n in range(N):
-    ybb[:, n] = y[:, :, n].reshape(Nr * Ns)
-    Omega[:, :, n] = np.kron((Ut.conj().T @ F[:, :, n]).conj().T, Ur)
+    ybb[:, n] = y[:, :, n].reshape(Nr * Ns, order="F")
+    Omega[:, :, n] = np.kron((Ut.conj().T @ F[:, :, n]).T, Ur)
+
+# Run DCS-SOMP
+indices, h_hat = DCSSOMP(ybb, Omega, L)  # the last input is the number of paths it recovers
+
+# Estimate the RX position (here only with LOS)
+# compute the distance
+distances = np.zeros(L)
+
+for l in range(1, L + 1):
+    phases = np.unwrap(np.angle(h_hat[l - 1, :]))
+    distances[l - 1] = -np.mean(np.diff(phases)) * (N * Ts) * c / (2 * np.pi)
+
+    if distances[l - 1] < 0:
+        distances[l - 1] += N * Ts * c
+
+# determine LOS path index
+min_i = np.argmin(distances)
+
+# map indices back to angle
+index1 = np.ceil((indices + 1) / Nb).astype(int) - 1
+index2 = indices - index1 * Nb
+AOD_hat = np.arcsin(aa[index1])
+AOA_hat = np.pi * np.sign(np.arcsin(aa[index2])) - np.arcsin(aa[index2])
+
+# compute position
+posRx_hat = distances[min_i] * np.array([np.cos(AOD_hat[min_i]), np.sin(AOD_hat[min_i])])
+alpha_hat = np.mod(AOD_hat[min_i] - AOA_hat[min_i] - np.pi, np.pi)
+
+localizationError = np.linalg.norm(posRx_hat - posRx)
+orientationError = np.linalg.norm(alpha_hat - alpha)
+
+print("Localization Error:", localizationError, "Orientation Error:", orientationError)
