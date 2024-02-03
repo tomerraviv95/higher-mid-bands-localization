dbb781d73ecdff5afd57205c1abb692c5e8a10ca
tomerraviv95 <my@gmail.com>
2024-02-02 21:55:11 -0500

    Documenting all classes

--- python_code/optimization/__init__.py
+++ python_code/optimization/__init__.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 from python_code import conf
+from python_code.estimation import Estimation
 from python_code.optimization.position_optimizer_2d import optimize_to_estimate_position_2d
 from python_code.optimization.position_optimizer_3d import optimize_to_estimate_position_3d
 from python_code.utils.constants import DimensionType
 
 
-def optimize_to_estimate_position(bs_locs, estimation):
+def optimize_to_estimate_position(bs_locs: np.ndarray, estimation: Estimation) -> np.ndarray:
     if conf.dimensions == DimensionType.Two.name:
         est_ue_pos = optimize_to_estimate_position_2d(bs_locs, estimation)
     else:
