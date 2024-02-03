d438205e3a479ef00cb3b9087a1bff8e1c98af20
tomerraviv95 <my@gmail.com>
2024-02-03 14:59:29 -0500

    Documenting all classes

--- dir_definitions.py
+++ dir_definitions.py
@@ -4,6 +4,6 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
 CODE_DIR = os.path.join(ROOT_DIR, 'python_code')
 RESULTS_DIR = os.path.join(ROOT_DIR, 'results')
+
 # subfolders
 CONFIG_PATH = os.path.join(CODE_DIR, 'config.yaml')
-FIGURES_DIR = os.path.join(RESULTS_DIR, 'figures')
