0263a82f2086c6ca5100b71b3c10534ed629c3d4
tomerraviv95 <my@gmail.com>
2024-02-07 17:54:51 -0500

    updated

--- python_code/channel/generate_2d_channel.py
+++ python_code/channel/generate_2d_channel.py
@@ -20,30 +20,29 @@ def compute_observations(TOA: List[float], AOA: List[float], POWER: List[float],
     Ns = int(band.Nr_x * band.K * DATA_COEF)
     # Initialize the observations and beamformers
     y = np.zeros((band.Nr_x, band.K, Ns), dtype=complex)
-    # Generate multiple samples
-    for ns in range(Ns):
-        # Generate channel
-        h = np.zeros((band.Nr_x, band.K), dtype=complex)
-        # for each path
-        for l in range(L):
-            # assume random phase beamforming
-            F = np.exp(1j * np.random.rand(1) * 2 * np.pi)
-            # phase delay for the K subcarriers
-            delays_phase_vector = compute_time_options(band.fc, band.K, band.BW, np.array([TOA[l]]))
-            if conf.channel_bandwidth == ChannelBWType.NARROWBAND.name:
-                # different phase in each antennas element
-                aoa_vector = compute_angle_options(np.sin(np.array([AOA[l]])), zoa=1, values=np.arange(band.Nr_x)).T
-                delay_aoa_matrix = np.matmul(aoa_vector, delays_phase_vector)
-            elif conf.channel_bandwidth == ChannelBWType.WIDEBAND.name:
-                raise ValueError("Wideband is currently not supported!!")
-            else:
-                raise ValueError("No such type of channel BW!")
-            # add for each path
-            h += F * POWER[l] * delay_aoa_matrix
-        # adding the white Gaussian noise
-        noise = conf.sigma / np.sqrt(2) * (np.random.randn(band.Nr_x, band.K) + 1j * np.random.randn(band.Nr_x, band.K))
-        # finally sum up to y, the final observation
-        y[:, :, ns] = h + noise
+    # Generate channel
+    h = np.zeros((band.Nr_x, band.K, Ns), dtype=complex)
+    # for each path
+    for l in range(L):
+        # assume random phase beamforming
+        F = np.exp(1j * np.random.rand(1, 1, Ns) * 2 * np.pi)
+        # phase delay for the K subcarriers
+        delays_phase_vector = compute_time_options(band.fc, band.K, band.BW, np.array([TOA[l]]))
+        if conf.channel_bandwidth == ChannelBWType.NARROWBAND.name:
+            # different phase in each antennas element
+            aoa_vector = compute_angle_options(np.sin(np.array([AOA[l]])), zoa=1, values=np.arange(band.Nr_x)).T
+            delay_aoa_matrix = np.matmul(aoa_vector, delays_phase_vector)
+        elif conf.channel_bandwidth == ChannelBWType.WIDEBAND.name:
+            raise ValueError("Wideband is currently not supported!!")
+        else:
+            raise ValueError("No such type of channel BW!")
+        # add for each path
+        h += F * POWER[l] * delay_aoa_matrix[..., np.newaxis]
+    # adding the white Gaussian noise
+    noise = conf.sigma / np.sqrt(2) * (
+                np.random.randn(band.Nr_x, band.K, Ns) + 1j * np.random.randn(band.Nr_x, band.K, Ns))
+    # finally sum up to y, the final observation
+    y = h + noise
     return y
 
 
