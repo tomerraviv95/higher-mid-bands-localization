39991afef6bc0eafca5b18667bea95468932a358
tomer <tomerraviv95@gmail.com>
2024-02-08 14:46:41 +0200

    started working on the rmse for ny

---
+++ python_code/plotting/nyu_localization_rmse.py
@@ -0,0 +1,30 @@
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from python_code import conf
+from python_code.main import main
+
+plt.style.use('dark_background')
+
+if __name__ == "__main__":
+    rmse_list = []
+    ue_x_positions = range(0, 1121, 5)
+    ue_y_positions = range(0, 506, 5)
+    for ue_pos_x in ue_x_positions:
+        rmse_list.append([])
+        for ue_pos_y in ue_y_positions:
+            print('******' * 5)
+            conf.ue_pos[0] = ue_pos_x
+            conf.ue_pos[1] = ue_pos_y
+            rmse = main()
+            rmse_list[-1].append(rmse)
+    rmse_array = np.flipud(np.round(np.array(rmse_list), 2).T)
+    fig = plt.figure(figsize=(12, 8), dpi=80)
+    sns.heatmap(rmse_array, annot=True, linewidths=.5, vmin=0, vmax=1)
+    ax = plt.gca()
+    plt.xlabel('X location')
+    plt.xticks(range(len(ue_x_positions)), ue_x_positions)
+    plt.ylabel('Y location')
+    plt.yticks(range(1, len(ue_y_positions) + 1), ue_y_positions[::-1])
+    plt.savefig(f'rmse_map_{conf.fc}_{conf.Nr_x}_{conf.BW}.png', dpi=fig.dpi)
