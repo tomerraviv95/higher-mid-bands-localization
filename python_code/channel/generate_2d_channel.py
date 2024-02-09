now uploaded the correct power calculation

python_code/channel/generate_2d_channel.py
@@ -40,12 +40,12 @@ def compute_observations(TOA: List[float], AOA: List[float], POWER: List[float],
                 delay_aoa_tensor = torch.tensor(F, dtype=torch.cfloat).to(
                     DEVICE) * delay_aoa_tensor.unsqueeze(-1)
                 # add for each path
-                h += (POWER[l] / POWER[0]) * delay_aoa_tensor
+                h += watt_from_dbm(POWER[l]) * delay_aoa_tensor
             else:
                 delay_aoa_matrix = np.matmul(aoa_vector, delays_phase_vector)
                 delay_aoa_matrix = F * delay_aoa_matrix[..., np.newaxis]
                 # add for each path
-                h += (POWER[l] / POWER[0]) * delay_aoa_matrix
+                h += watt_from_dbm(POWER[l]) * delay_aoa_matrix
         else:
             raise ValueError("No such type of channel BW!")
     if torch.cuda.is_available():
@@ -54,10 +54,12 @@ def compute_observations(TOA: List[float], AOA: List[float], POWER: List[float],
     normal_gaussian_noise = 1 / np.sqrt(2) * (
             np.random.randn(band.Nr_x, band.K, Ns) + 1j * np.random.randn(band.Nr_x, band.K, Ns))
     # calculate the SNR
-    noise_power = watt_from_dbm(NOISE_PSD) * band.BW * MEGA
-    power = watt_from_dbm(conf.input_power)
+    NF = 7  # noise figure in dB
+    BW_loss = 10 * np.log10(band.BW * MEGA) # BW loss
+    N_0 = -174  # dBm
+    noise_power = watt_from_dbm(NF + BW_loss + N_0)
     # finally sum up to y, the final observation
-    y = (power / noise_power) * POWER[0] * h + normal_gaussian_noise
+    y = (1 / noise_power) * h + normal_gaussian_noise
     return y
 
 

python_code/channel/ny_channel/ny_channel_loader_2d.py
@@ -8,7 +8,6 @@ from dir_definitions import RAYTRACING_DIR
 from python_code import conf
 from python_code.utils.bands_manipulation import Band
 from python_code.utils.constants import MU_SEC
-from python_code.utils.path_loss import watt_from_db
 
 
 def load_ny_scenario(bs_ind: int, ue_pos: np.ndarray, band: Band):
@@ -22,9 +21,9 @@ def load_ny_scenario(bs_ind: int, ue_pos: np.ndarray, band: Band):
     n_paths = row['n_path'].astype(int)
     powers, toas, aoas = [], [], []
     for path in range(1, n_paths + 1):
-        initial_power = 0.5 * (1 + 1j)
+        initial_power = conf.input_power  # initial power in dBm
         loss_db = row[f'path_loss_{path}']
-        received_power = initial_power * (1 / watt_from_db(loss_db))
+        received_power = initial_power - loss_db  # still in dBm
         toa = row[f'delay_{path}'] / MU_SEC
         if path == 1:
             conf.medium_speed = np.linalg.norm(ue_pos - bs_loc) / toa