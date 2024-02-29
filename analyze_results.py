06e5099d57d16163776c3f5dae26236a6640aba4
tomer <tomerraviv95@gmail.com>
2024-02-29 23:34:56 +0200

    added music

--- analyze_results.py
+++ analyze_results.py
@@ -25,6 +25,7 @@ file_to_label = {"fc_[6000]_antennas_[8]_bw_[2.4]_subcarriers_[20]_band_type_SIN
                  "fc_[18000]_antennas_[24]_bw_[4.8]_subcarriers_[20]_band_type_SINGLE.csv": "18GHz Sub-band Capon",
                  "fc_[24000]_antennas_[32]_bw_[9.6]_subcarriers_[20]_band_type_SINGLE.csv": "24GHz Sub-band Capon",
                  "fc_[6000, 24000]_antennas_[8, 32]_bw_[2.4, 9.6]_subcarriers_[20, 20]_band_type_MULTI.csv": "{6,24}GHz Multi-Frequency Capon",
+                 "MUSIC_fc_[6000, 24000]_antennas_[8, 32]_bw_[2.4, 9.6]_subcarriers_[20, 20]_band_type_MULTI.csv": "{6,24}GHz Multi-Frequency MUSIC",
                  "fc_[6000, 12000, 18000, 24000]_antennas_[8, 16, 24, 32]_bw_[2.4, 3.6, 4.8, 9.6]_subcarriers_[20, 20, 20, 20]_band_type_MULTI.csv": "{6,12,18,24}GHz Multi-Frequency Capon"}
 
 color_to_label = {"fc_[6000]_antennas_[8]_bw_[2.4]_subcarriers_[20]_band_type_SINGLE.csv": "blue",
@@ -32,6 +33,7 @@ color_to_label = {"fc_[6000]_antennas_[8]_bw_[2.4]_subcarriers_[20]_band_type_SI
                   "fc_[18000]_antennas_[24]_bw_[4.8]_subcarriers_[20]_band_type_SINGLE.csv": "orange",
                   "fc_[24000]_antennas_[32]_bw_[9.6]_subcarriers_[20]_band_type_SINGLE.csv": "red",
                   "fc_[6000, 24000]_antennas_[8, 32]_bw_[2.4, 9.6]_subcarriers_[20, 20]_band_type_MULTI.csv": "purple",
+                  "MUSIC_fc_[6000, 24000]_antennas_[8, 32]_bw_[2.4, 9.6]_subcarriers_[20, 20]_band_type_MULTI.csv": "pink",
                   "fc_[6000, 12000, 18000, 24000]_antennas_[8, 16, 24, 32]_bw_[2.4, 3.6, 4.8, 9.6]_subcarriers_[20, 20, 20, 20]_band_type_MULTI.csv": "black"}
 
 marker_to_label = {"fc_[6000]_antennas_[8]_bw_[2.4]_subcarriers_[20]_band_type_SINGLE.csv": "o",
@@ -39,6 +41,7 @@ marker_to_label = {"fc_[6000]_antennas_[8]_bw_[2.4]_subcarriers_[20]_band_type_S
                    "fc_[18000]_antennas_[24]_bw_[4.8]_subcarriers_[20]_band_type_SINGLE.csv": "s",
                    "fc_[24000]_antennas_[32]_bw_[9.6]_subcarriers_[20]_band_type_SINGLE.csv": "p",
                    "fc_[6000, 24000]_antennas_[8, 32]_bw_[2.4, 9.6]_subcarriers_[20, 20]_band_type_MULTI.csv": "^",
+                   "MUSIC_fc_[6000, 24000]_antennas_[8, 32]_bw_[2.4, 9.6]_subcarriers_[20, 20]_band_type_MULTI.csv": ">",
                    "fc_[6000, 12000, 18000, 24000]_antennas_[8, 16, 24, 32]_bw_[2.4, 3.6, 4.8, 9.6]_subcarriers_[20, 20, 20, 20]_band_type_MULTI.csv": "P"}
 
 linestyle_to_label = {"fc_[6000]_antennas_[8]_bw_[2.4]_subcarriers_[20]_band_type_SINGLE.csv": "dotted",
@@ -46,6 +49,7 @@ linestyle_to_label = {"fc_[6000]_antennas_[8]_bw_[2.4]_subcarriers_[20]_band_typ
                    "fc_[18000]_antennas_[24]_bw_[4.8]_subcarriers_[20]_band_type_SINGLE.csv": "dashdot",
                    "fc_[24000]_antennas_[32]_bw_[9.6]_subcarriers_[20]_band_type_SINGLE.csv": (5,(10,3)),
                    "fc_[6000, 24000]_antennas_[8, 32]_bw_[2.4, 9.6]_subcarriers_[20, 20]_band_type_MULTI.csv": "solid",
+                   "MUSIC_fc_[6000, 24000]_antennas_[8, 32]_bw_[2.4, 9.6]_subcarriers_[20, 20]_band_type_MULTI.csv": "solid",
                    "fc_[6000, 12000, 18000, 24000]_antennas_[8, 16, 24, 32]_bw_[2.4, 3.6, 4.8, 9.6]_subcarriers_[20, 20, 20, 20]_band_type_MULTI.csv": "solid"}
 
 
@@ -62,8 +66,9 @@ if __name__ == "__main__":
         "fc_[6000]_antennas_[8]_bw_[2.4]_subcarriers_[20]_band_type_SINGLE.csv",
         "fc_[24000]_antennas_[32]_bw_[9.6]_subcarriers_[20]_band_type_SINGLE.csv",
         "fc_[6000, 24000]_antennas_[8, 32]_bw_[2.4, 9.6]_subcarriers_[20, 20]_band_type_MULTI.csv",
+        "MUSIC_fc_[6000, 24000]_antennas_[8, 32]_bw_[2.4, 9.6]_subcarriers_[20, 20]_band_type_MULTI.csv"
     ]
-    files = files1
+    files = files2
     mean_rmse_dict = {}
     for input_power in input_powers:
         dir_path = f"{NY_DIR}/{str(input_power)}/"
