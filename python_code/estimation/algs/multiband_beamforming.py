779a00716d68ddbcee08b0f621c67de2acb182f7
tomerraviv95 <my@gmail.com>
2024-04-16 23:55:57 -0400

    updated the map plotter

--- python_code/estimation/algs/multiband_beamforming.py
+++ python_code/estimation/algs/multiband_beamforming.py
@@ -17,21 +17,18 @@ class MultiBandBeamformer(Beamformer):
     def run(self, y: List[np.ndarray], basis_vectors: List[np.ndarray], second_dim: bool = False,
             use_gpu: bool = False) -> Tuple[np.ndarray, np.ndarray]:
         K = len(y)
-        peak, chosen_k = None, None
         norm_values_list = []
+        peaks= []
+        peak_values = []
         for k in range(K):
             # compute the spectrum values for sub-band
             norm_values = self._compute_beamforming_spectrum(basis_vectors[k], use_gpu, y[k])
             maximum_ind = np.array(np.unravel_index(np.argmax(norm_values, axis=None), norm_values.shape))
             norm_values_list.append(norm_values)
-            # only if peak is above noise level
-            print(k, norm_values[maximum_ind[0], maximum_ind[1]] / np.mean(norm_values), norm_values[maximum_ind[0], maximum_ind[1]])
-            if norm_values[maximum_ind[0], maximum_ind[1]] > ALG_THRESHOLD * np.mean(norm_values):
-                peak = maximum_ind
-                chosen_k = k
-        # if all are below the noise level - choose the last sub-band
-        if chosen_k is None:
-            peak = maximum_ind
-            chosen_k = K - 1
-        self.k = chosen_k
+            peaks.append(maximum_ind)
+            peak_values.append(norm_values[maximum_ind[0], maximum_ind[1]])
+            print(norm_values[maximum_ind[0], maximum_ind[1]]/np.mean(norm_values))
+        max_peak_value = max(peak_values)
+        self.k = peak_values.index(max_peak_value)
+        peak = peaks[self.k]
         return np.array([peak]), norm_values_list[self.k]
